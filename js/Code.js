c457ca1c8fc74f4c5a2a3a2593bd6fb02c1373ad
V-S-I <77443279+Inieyaa@users.noreply.github.com>
2022-08-15 19:48:17 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "EGGS";
+const accessCode2 = "CANDLE";
+const accessCode3 = "ALL MONTHS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("What has to be broken before you can use it?", 100,50)
+    fill("lightblue")
+    text("Hint: Cooks use these mostly", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Im tall when Im young, and Im short when Im old. What am I?", 700,150)
+    fill("lightblue")
+    text("Hint: It gives u light in darkness", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("What month of the year has 28 days?", 100,250)
+    fill("lightblue")
+    text("Hint: Dude, its so easy -_-", 100,270)
+
+}
